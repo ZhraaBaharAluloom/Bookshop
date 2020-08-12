da29e30fa1ee8f56c2b411714eab5590119b95c5
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2020-08-11 18:01:37 -0700

    roles & permision

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -2,38 +2,33 @@ import { decorate, observable } from "mobx";
 import instance from "./instance";
 import decode from "jwt-decode";
 
-
 class AuthStore {
-user = null;
+  user = null;
 
-    signup = async (userData) => {
-        try {
-       const res=  await instance.post("/signup", userData);
-       console.log("AuthStore -> signup -> res", res.data)
-       
-        } catch (error) {
-        console.log("AuthStore -> signup -> error", error);
-        }
-    };
+  signup = async (userData) => {
+    try {
+      const res = await instance.post("/signup", userData);
+      this.user = decode(res.data.token);
 
-
-    signin = async (userData) => {
-        try {
-            const res = await instance.post("/signin", userData);
-            this.user = decode(res.data.token)
-            console.log("AuthStore -> signin -> this.user ", this.user )
-        } catch (error) {
-        console.log("AuthStore -> signin -> error", error)
-            
-        }
-
+      console.log("AuthStore -> signup -> res", res.data);
+    } catch (error) {
+      console.log("AuthStore -> signup -> error", error);
     }
+  };
+
+  signin = async (userData) => {
+    try {
+      const res = await instance.post("/signin", userData);
+      this.user = decode(res.data.token);
+      console.log("AuthStore -> signin -> this.user ", this.user);
+    } catch (error) {
+      console.log("AuthStore -> signin -> error", error);
+    }
+  };
 }
 
-
-
 decorate(AuthStore, {
-user: observable,
+  user: observable,
 });
 
 const authStore = new AuthStore();
