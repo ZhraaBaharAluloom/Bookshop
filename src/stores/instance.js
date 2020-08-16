dd70f123c515f4f58333a1c63b87b202ff01ab6a
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2020-08-16 16:01:24 -0700

    added a new url

--- src/stores/instance.js
+++ src/stores/instance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 const instance = axios.create({
-    baseURL:"http://localhost:8000",
+  baseURL: "https://secret-tundra-53961.herokuapp.com",
 });
 
 export default instance;
