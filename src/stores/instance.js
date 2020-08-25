7bf79b513cfa4adda8975250d025758276c24b7d
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2020-08-24 20:51:17 -0700

    change the in URL

--- src/stores/instance.js
+++ src/stores/instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://secret-tundra-53961.herokuapp.com",
 });
 
 export default instance;
